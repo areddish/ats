8c73e0a347c07374cdd808f36088ac35dece0889
Andrew Reddish <areddish@foosion.net>
2018-02-05 15:22:38 -0500

    Add an asset with a sample UT

---
+++ ats/assets/stock.py
@@ -0,0 +1,23 @@
+from ibapi.contract import *
+
+# These symbols require an exchange to be set otherwise we get an error since there
+# are duplicates in the IB system. These are empirically found by getting an error
+# when requesting a quote.
+# https://pennies.interactivebrokers.com/cstools/contract_info/v3.9/index.php can be
+# used to look up a symbol and if it has multiple stocks listed with the same name
+# and different exchanges we need to specify an exchange.
+# Also see: http://interactivebrokers.github.io/tws-api/basic_contracts.html#gsc.tab=0
+tickers_that_need_primary_exchange = {
+    "MSFT": "NASDAQ",
+    "CSCO": "NASDAQ",
+    "INTC": "NASDAQ"
+}
+
+
+class Stock(Contract):
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.secType = "STK"
+        if symbol in tickers_that_need_primary_exchange:
+            self.primaryExchange = tickers_that_need_primary_exchange[symbol]
+    
---
+++ test/asset_tests.py
@@ -0,0 +1,15 @@
+import unittest
+from ats.assets import stock
+
+
+class TestAssets(unittest.TestCase):
+    def test_primaryExchange(self):
+        s = stock.Stock("MSFT")
+        self.assertEqual(s.primaryExchange, "NASDAQ")
+
+    def test_symbol(self):
+        s = stock.Stock("MSFT")
+        self.assertEqual(s.symbol, "MSFT")
+
+if "__main__" == __name__:
+    unittest.main()
