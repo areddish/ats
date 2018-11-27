d7b69100c0f688430c4a6169715625ea7792debd
Andrew Reddish <areddish@foosion.net>
2018-11-27 17:03:57 -0500

    max timestamp

--- downloader/max_timestamp.py
+++ downloader/max_timestamp.py
@@ -7,6 +7,23 @@ from ats.assets import Stock
 
 import argparse
 
+  
+class HistoricalHeadTimeStampRequest:
+    def __init__(self, broker, symbol):
+        self.broker = broker
+        self.contract = Stock(symbol)
+    
+    def make_request(self):
+        self.reqId = broker.register_request(self)
+        broker.reqHeadTimeStamp(self.reqId, self.contract)
+
+    def on_response(reqId, timestamp):
+        if (self.reqId != reqId):
+            raise "Invalid request"
+
+        print (timestamp)
+        broker.cancelHeadTimeStamp(self.reqId)
+
 if "__main__" == __name__:
 
     arg_parser = argparse.ArgumentParser()
@@ -18,16 +35,20 @@ if "__main__" == __name__:
     args = arg_parser.parse_args()
 
     symbols = [ "NFLX", "MSFT", "AMZN", "SPX", "GOOG", "AAPL", "SPY", "TNA", "TSLA" ]
+    symbols_map = dict(zip(range(len(symbols)), symbols))
 
     try:
         broker = BrokerPlatform(args.port, args.id, args.data_dir)
         broker.connect()
 
         def p(ts):
-            print (ts)
+            ts_int = int(ts)
+            time = datetime.datetime.fromtimestamp(ts_int)
+            print (symbols_map, ts, time.strftime("%m-%d-%Y %H:%M:%S"))
+            broker.cancelHeadTimeStamp(35)
 
         broker.register_historical_callback(35, p)
-        for s in symbols:
+        for s in zip(symbols, range(len(symboles)):
             print (s,end="")
             broker.reqHeadTimeStamp(35, Stock(s), "TRADES", 1, 2)
             time.sleep(3)
