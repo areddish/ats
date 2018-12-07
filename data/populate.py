566ffd58e0df324301119c1782192b5f51b1ff9e
Andrew Reddish <areddish@foosion.net>
2018-12-07 14:50:53 -0500

    Stub impl

---
+++ data/populate.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+def create_table():
+    # Create a table to hold data or return existing table
+    pass
+
+def insert_bar(table, dt, open, high, low, close, volume, bar_count):
+    # Insert a bar as a row
+    #c.execute("INSERT INTO ? VALUES (?, ?, ?, ?, ?, ?, ?)", table, dt, open, high, low, close, volume, bar_count)
+
+def insert_bars(table, bars)
+    # Insert a list of bars
+    #c.executemany("INSERT INTO ? VALUES (?, ?, ?, ?, ?, ?, ?)", table, bars)
+
+if __name__ == "__main__":
+    print("Creating")
+    connection = sqlite3.connect('stock.db')
+    c = connection.cursor()
+
+    # Create table, if missing
+    table_name = create_table()
+
+    # 1. find all files
+    # 2. for each file, parse out the lines and insert
+    
+    connection.commit()
+    connection.close()
