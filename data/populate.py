536c81d76cdafb983037f7827dc7f91e55f72cd8
Shivakumar Mahakali <mahakalim@yahoo.com>
2018-12-19 14:45:57 -0500

    Added : for proper python syntax. Added extra space between imports and function statements to follow PEP guidelines.

--- data/populate.py
+++ data/populate.py
@@ -1,16 +1,22 @@
 import sqlite3
 
+
 def create_table():
     # Create a table to hold data or return existing table
     pass
 
+
 def insert_bar(table, dt, open, high, low, close, volume, bar_count):
     # Insert a bar as a row
     #c.execute("INSERT INTO ? VALUES (?, ?, ?, ?, ?, ?, ?)", table, dt, open, high, low, close, volume, bar_count)
+    pass
 
-def insert_bars(table, bars)
+
+def insert_bars(table, bars):
     # Insert a list of bars
     #c.executemany("INSERT INTO ? VALUES (?, ?, ?, ?, ?, ?, ?)", table, bars)
+    pass
+
 
 if __name__ == "__main__":
     print("Creating")
