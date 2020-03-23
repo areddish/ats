93c15048431ced938d2e7f39d9d9cb5bfee41b97
Andrew Reddish <areddish@foosion.net>
2020-03-23 02:18:01 -0400

    Setup fixes

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='autotrader', version='1.0', packages=find_packages())
+setup(name='autotrader',
+      version='0.1',
+      description='AutoTrading platform',
+      url='http://github.com/areddish/ats',
+      author='Foosion Software',
+      author_email='areddish@foosion.net',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'pandas',
+          'twilio'
+      ],
+      zip_safe=False)
